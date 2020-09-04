2aa49163f8c1062c7f45f9589b0d232945b6f890
Jimmy <jimbrig2011@outlook.com>
2020-09-03 20:43:21 -0400

    document

--- inst/dependencies.R
+++ inst/dependencies.R
@@ -1,6 +1,6 @@
 # No Remotes ----
 # Attachments ----
-to_install <- c("dplyr", "fs", "fst", "htmltools", "magrittr", "openxlsx", "purrr", "rlang", "shiny", "tibble", "tidyr")
+to_install <- c("dplyr", "fs", "fst", "htmltools", "lubridate", "magrittr", "openxlsx", "purrr", "rlang", "shiny", "stringr", "tibble", "tidyr")
   for (i in to_install) {
     message(paste("looking for ", i))
     if (!requireNamespace(i)) {
