286da86e247b7660f1569069fa9d3afe950e79b8
Jimmy Briggs <jimmy.briggs@oliverwyman.com>
2020-10-12 23:56:16 -0400

    update docs

--- R/shiny-download_bttn.R
+++ R/shiny-download_bttn.R
@@ -25,17 +25,3 @@ download_bttn <- function(outputId, label = "Download", class = NULL, icon = NUL
       ...
     )
 }
-
-#' Ensures a text string is a valid email address
-#'
-#' @param x string
-#'
-#' @return logical
-#' @export
-is_valid_email <- function(x) {
-
-  grepl("\\<[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,}\\>",
-        as.character(x),
-        ignore.case = TRUE)
-
-}
