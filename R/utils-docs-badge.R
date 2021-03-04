4c27fb8af30dc6c8e2e87d2c982937449aeb82c7
Jimmy Briggs <jimbrig2011@outlook.com>
2021-03-04 16:54:47 -0500

    up

--- R/utils-docs-badge.R
+++ R/utils-docs-badge.R
@@ -7,9 +7,6 @@
 #'
 #' @return character string to be pasted into README
 #' @export
-#'
-#' @examples
-#' badge("Client", "Client XYZ", color = "black", url = "https://www.client-xyz.com/en-us")
 badge <- function(label, value, color, url) {
 
   # adjust value and label for any spaces
@@ -21,8 +18,3 @@ badge <- function(label, value, color, url) {
     value, "-", color, ".svg)](", url, ")"
   )
 }
-
-# TEST
-#
-# b <- badge("Client", "Client XYZ", color = "black", url = "https://www.client-xyz.com/en-us")
-# b == "[![client](https://img.shields.io/badge/Client-FedEx%20Express-black.svg)](https://www.fedex.com/en-us)"
